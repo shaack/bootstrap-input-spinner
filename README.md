eb372ab4fd2eff68e8506b31f57a8c017f96a3f9
shaack <shaack@gmail.com>
2018-03-14 10:09:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sb-bootstrap-input-spinner
-JavaScript ES6 Module to create a Bootstrap 4 input spinner element
+# sb-input-spinner
+
+Bootstrap/jQuery plugin to create input spinner elements with Bootstrap 4.
+
+## Usage
+
+```javascript
+$("#input-element").InputSpinner({min: 0, max: 100, step: 10});
+```
+
---
+++ src/sb-input-spinner/InputSpinner.js
@@ -0,0 +1,20 @@
+/**
+ * Author: shaack
+ * Date: 14.03.2018
+ */
+
+(function( $ ) {
+
+    $.fn.InputSpinner = function( action ) {
+
+        if ( action === "open") {
+            // Open popup code.
+        }
+
+        if ( action === "close" ) {
+            // Close popup code.
+        }
+
+    };
+
+}( jQuery ));
