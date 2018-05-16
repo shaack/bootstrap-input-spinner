aadfa49b5d194c0f9bf82e9bc72248f817fb0a49
shaack <shaack@gmail.com>
2018-05-16 10:04:49 +0200

    Optimization of "change" and "input" events

--- src/InputSpinner.js
+++ src/InputSpinner.js
@@ -4,8 +4,8 @@
  */
 
 (function ($) {
-    "use strict";
-    var spacePressed = false;
+    "use strict"
+    var spacePressed = false
     $.fn.InputSpinner = function (options) {
 
         var config = {
@@ -20,8 +20,8 @@
             boostThreshold: 15, // boost after these steps
             boostMultiplier: 4,
             locale: null // the locale for number rendering; if null, the browsers language is used
-        };
-        Object.assign(config, options);
+        }
+        Object.assign(config, options)
 
         var html = '<div class="input-group ' + config.groupClass + '">' +
             '<div class="input-group-prepend">' +
@@ -31,153 +31,156 @@
             '<div class="input-group-append">' +
             '<button style="min-width: ' + config.buttonsWidth + '" class="btn btn-increment ' + config.buttonsClass + '" type="button">' + config.incrementButton + '</button>' +
             '</div>' +
-            '</div>';
+            '</div>'
 
-        var locale = config.locale || navigator.language || "en-US";
+        var locale = config.locale || navigator.language || "en-US"
 
         this.each(function () {
 
-            var $original = $(this);
-            $original.hide();
+            var $original = $(this)
+            $original.hide()
 
-            var autoDelayHandler = null;
-            var autoIntervalHandler = null;
+            var autoDelayHandler = null
+            var autoIntervalHandler = null
 
-            var $inputGroup = $(html);
-            var $buttonDecrement = $inputGroup.find(".btn-decrement");
-            var $buttonIncrement = $inputGroup.find(".btn-increment");
-            var $input = $inputGroup.find("input");
+            var $inputGroup = $(html)
+            var $buttonDecrement = $inputGroup.find(".btn-decrement")
+            var $buttonIncrement = $inputGroup.find(".btn-increment")
+            var $input = $inputGroup.find("input")
 
-            var min = parseFloat($original.prop("min")) || 0;
-            var max = parseFloat($original.prop("max")) || Infinity;
-            var step = parseFloat($original.prop("step")) || 1;
-            var decimals = parseInt($original.attr("data-decimals")) || 0;
+            var min = parseFloat($original.prop("min")) || 0
+            var max = parseFloat($original.prop("max")) || Infinity
+            var step = parseFloat($original.prop("step")) || 1
+            var decimals = parseInt($original.attr("data-decimals")) || 0
 
-            var numberFormat = new Intl.NumberFormat(locale, {minimumFractionDigits: decimals});
-            var value = parseFloat($original.val());
+            var numberFormat = new Intl.NumberFormat(locale, {minimumFractionDigits: decimals})
+            var value = parseFloat($original.val())
 
-            if($original.prop("class").indexOf("is-invalid") !== -1) {
-                $input.addClass("is-invalid");
+            if ($original.prop("class").indexOf("is-invalid") !== -1) {
+                $input.addClass("is-invalid")
             }
-            if($original.prop("class").indexOf("is-valid") !== -1) {
-                $input.addClass("is-valid");
+            if ($original.prop("class").indexOf("is-valid") !== -1) {
+                $input.addClass("is-valid")
             }
-            if($original.prop("required")) {
-                $input.prop("required", true);
+            if ($original.prop("required")) {
+                $input.prop("required", true)
             }
-            if($original.prop("placeholder")) {
-                $input.prop("placeholder", $original.prop("placeholder"));
+            if ($original.prop("placeholder")) {
+                $input.prop("placeholder", $original.prop("placeholder"))
             }
 
-            $original.after($inputGroup);
+            $original.after($inputGroup)
 
-            if(isNaN(value)) {
-                $original.val("");
-                $input.val("");
+            if (isNaN(value)) {
+                $original.val("")
+                $input.val("")
             } else {
-                $original.val(value);
-                $input.val(numberFormat.format(value));
+                $original.val(value)
+                $input.val(numberFormat.format(value))
             }
 
-            var boostCount = 0;
+            var boostCount = 0
 
             $input.on("paste keyup change", function () {
-                var inputValue = $input.val();
+                var inputValue = $input.val()
                 if (locale === "en-US" || locale === "en-GB" || locale === "th-TH") {
-                    value = parseFloat(inputValue);
+                    value = parseFloat(inputValue)
                 } else {
-                    value = parseFloat(inputValue.replace(/[. ]/g, '').replace(/,/g, '.')); // i18n
+                    value = parseFloat(inputValue.replace(/[. ]/g, '').replace(/,/g, '.')) // i18n
                 }
-                if(isNaN(value)) {
-                    $original.val("");
+                if (isNaN(value)) {
+                    $original.val("")
                 } else {
-                    $original.val(value);
+                    $original.val(value)
                 }
-                $original.trigger("change")
-                $original.trigger("input")
-            });
+                triggerChange($original)
+            })
 
             onPointerDown($buttonDecrement[0], function () {
-                stepHandling(-step);
-                $original.trigger("change")
-                $original.trigger("input")
-            });
+                stepHandling(-step)
+            })
 
             onPointerDown($buttonIncrement[0], function () {
-                stepHandling(step);
-                $original.trigger("change")
-                $original.trigger("input")
-            });
+                stepHandling(step)
+            })
 
             onPointerUp(document.body, function () {
-                resetTimer();
-            });
+                resetTimer()
+            })
+
+            function triggerChange($element) {
+                var changeEvent = new Event("change", {bubbles: true})
+                var inputEvent = new Event("input", {bubbles: true})
+                $element[0].dispatchEvent(changeEvent)
+                $element[0].dispatchEvent(inputEvent)
+            }
 
             function stepHandling(step) {
-                calcStep(step);
-                resetTimer();
+                calcStep(step)
+                resetTimer()
                 autoDelayHandler = setTimeout(function () {
                     autoIntervalHandler = setInterval(function () {
                         if (boostCount > config.boostThreshold) {
-                            calcStep(step * config.boostMultiplier);
+                            calcStep(step * config.boostMultiplier)
                         } else {
-                            calcStep(step);
+                            calcStep(step)
                         }
-                        boostCount++;
-                    }, config.autoInterval);
-                }, config.autoDelay);
+                        boostCount++
+                    }, config.autoInterval)
+                }, config.autoDelay)
             }
 
             function calcStep(step) {
-                if(isNaN(value)) {
-                    value = 0;
+                if (isNaN(value)) {
+                    value = 0
                 }
-                value = Math.round(value / step) * step;
-                value = Math.min(Math.max(value + step, min), max);
-                $input.val(numberFormat.format(value));
-                $original.val(Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals));
+                value = Math.round(value / step) * step
+                value = Math.min(Math.max(value + step, min), max)
+                $input.val(numberFormat.format(value))
+                $original.val(Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals))
+                triggerChange($original)
             }
 
             function resetTimer() {
-                boostCount = 0;
-                clearTimeout(autoDelayHandler);
-                clearTimeout(autoIntervalHandler);
+                boostCount = 0
+                clearTimeout(autoDelayHandler)
+                clearTimeout(autoIntervalHandler)
             }
 
-        });
+        })
 
-    };
+    }
 
     function onPointerUp(element, callback) {
         element.addEventListener("mouseup", function (e) {
-            callback(e);
-        });
+            callback(e)
+        })
         element.addEventListener("touchend", function (e) {
-            callback(e);
-        });
+            callback(e)
+        })
         element.addEventListener("keyup", function (e) {
-            if(e.keyCode === 32) {
+            if (e.keyCode === 32) {
                 spacePressed = false
-                callback(e);
+                callback(e)
             }
-        });
+        })
     }
 
     function onPointerDown(element, callback) {
         element.addEventListener("mousedown", function (e) {
-            e.preventDefault();
-            callback(e);
-        });
+            e.preventDefault()
+            callback(e)
+        })
         element.addEventListener("touchstart", function (e) {
-            e.preventDefault();
-            callback(e);
-        });
+            e.preventDefault()
+            callback(e)
+        })
         element.addEventListener("keydown", function (e) {
-            if(e.keyCode === 32 && !spacePressed) {
+            if (e.keyCode === 32 && !spacePressed) {
                 spacePressed = true
-                callback(e);
+                callback(e)
             }
-        });
+        })
     }
 
-}(jQuery));
+}(jQuery))
